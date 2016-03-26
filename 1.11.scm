ec6ac3d37fac6908e916a74fa9322b86e72eaaf7
Marcos Savoury <marcossavoury@gmail.com>
2016-03-26 10:53:56 -0400

    Adding 1.11

--- 1.11.scm
+++ 1.11.scm
@@ -1,11 +1,16 @@
-(define (pascal row col)
-  (cond
-	((= col 1) 1)
-	((= row col) 1)
-	(else (+ (pascal (- row 1) (- col 1))
-			 (pascal (- row 1) col) 
-	      )
-    )
+(define (func-recur n)
+	(if (< n 3)
+		n
+		(+ (func-recur (- n 1))
+		   (* 2 (func-recur (- n 2)))
+		   (* 3 (func-recur (- n 3))))))
 
-  )
+(define (func-iter n)
+
+  (define (func-iter-inner a b c n)
+	(cond   ((= n 0) a)
+		((= n 1) b)
+		((= n 2) c)
+		(else (func-iter-inner b c (+ c (* 2 b) (* 3 a))(- n 1)))))	
+  (func-iter-inner 0 1 2 n)
 )
